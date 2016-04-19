967e9e4fac42af7623db6cf7b1de36b259a5b1d8
Sanjeevan P <sanjeevan.premkumar@gmail.com>
2016-04-19 13:51:19 -0400

    Update login.php

--- login.php
+++ login.php
@@ -4,7 +4,6 @@
 <title>OPP App</title>
 
 <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places&sensor=false"></script>
 <!--Import Google Icon Font-->
 <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <!--Import materialize.css-->
@@ -18,7 +17,7 @@
 <body id="login">
 
 	<div class="container">
-	  <div class="logo center"><img src="http://localhost:81/oppApp-master/img/logo.png" alt="OPP Logo" /></div>
+	  <div class="logo center"><img src="/img/logo.png" alt="OPP Logo" /></div>
 			<form class="form col s12" method="post" action="validate.php">
 			<div class="row">
 				<div class="input-field col s12">
@@ -42,6 +41,5 @@
 
 <!--Import jQuery before materialize.js-->
 <script type="text/javascript" src="js/materialize.min.js"></script>
-<script type="text/javascript" src="js/menu.js"></script>
 
 </html>
