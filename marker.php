f96a158b9447d4a65bcfd3177e8f6d5160e838f7
PAMP Solutions <pampsolutions@outlook.com>
2016-04-20 16:12:54 -0400

    Update marker v1.01

--- marker.php
+++ marker.php
@@ -1,56 +1,43 @@
-<?php ob_start();
+<?php ob_start(); 
 
 require_once('header.php');
 
-?>
-
-<div id="editMarker">
-  <div class="row">
-    <h1>Dashboard</h1>
-  </div>
-
-<?php
-
 if (isset($_GET['id'])) {
 	$id = $_GET['id'];
-
+	
 	try {
 	    require_once('db.php');
-
+	    	    
 	    $sql = "SELECT * FROM markers WHERE id = :id";
 	    $cmd = $conn->prepare($sql);
 	    $cmd->bindParam(':id', $id, PDO::PARAM_INT);
 	    $cmd->execute();
 	    $result = $cmd->fetchAll();
-
+	
 	    foreach ($result as $row) {
 	    	$name = $row['name'];
 	    	$date = $row['date'];
 	    	$city = $row['city'];
             $address = $row['address'];
+            $type = $row['type'];
 	    }
-
+	   
 	    //disconnect
 	    $conn = null;
 	}
 	catch (Exception $e) {
 		mail('pampsolutions@outlook.com', 'App Error', $e, 'From: pampsolutions@outlook.com');
-
+		
 		header('location:error.php');
 		exit();
 	}
 }
 ?>
-
-<div class="container">
-
-<h2>Edit a Marker</h2>
-
-<div class="card edit-card">
+<div class="card">
 <form method="post" action="save-marker.php">
 
 <div>
-    <label for="name">Descriptive Title:</label>
+    <label for="name">Name:</label>
     <input name="name" required value="<?php echo $name; ?>" />
 </div>
 <div>
@@ -58,26 +45,26 @@ if (isset($_GET['id'])) {
     <input name="date" required value="<?php echo $date; ?>" />
 </div>
 <div>
-    <label for="city">Main Intersection:</label>
+    <label for="city">City:</label>
     <input name="city" required value="<?php echo $city; ?>" />
 </div>
 <div>
-    <label for="address">Details:</label>
+    <label for="address">Description:</label>
     <input name="address" required value="<?php echo $address; ?>" />
 </div>
+<div>
+    <label for="type">Type:</label>
+    <input name="type" required value="<?php echo $type; ?>" />
+</div>
 <input type="hidden" name="id" value="<?php echo $id; ?>" />
-<input type="submit" class="waves-effect waves-light btn" value="Save" />
+<input type="submit" class="waves-effect waves-light btn green" value="Save" />
 
 </form>
 </div>
 
-</div><!-- .container -->
-
-</div><!-- #dashboard -->
-
-<?php
+<?php 
 //embed footer
-require_once('footer.php');
+require_once('footer.php'); 
 
 ob_flush();
 ?>
