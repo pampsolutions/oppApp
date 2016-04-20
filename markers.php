b1f9c3d4b038e6ce3a7512ca7a8f0b4a5cd35953
PAMP Solutions <pampsolutions@outlook.com>
2016-04-20 18:41:58 -0400

    update markers v1.1

--- markers.php
+++ markers.php
@@ -19,6 +19,10 @@ $sql = "SELECT * FROM markers ORDER BY id DESC";
 
 $result = $conn->query($sql);
 
+$typeO = '<span class="orange relative" style="padding:5px 20px; border-radius:100%;">' . $types . '</span>';
+$typeG = '<span class="green relative" style="padding:5px 20px; border-radius:100%;">' . $types . '</span>';
+$typeR = '<span class="red relative" style="padding:5px 20px; border-radius:100%;">' . $types . '</span>';
+
 echo '
       <div class="row container">
       <div class="col s12">
@@ -27,11 +31,22 @@ echo '
         <thead><tr><th>Name</th><th>Date</th><th>City</th><th>Description</th><th>Type</th><th>Edit</th><th>Delete</th></tr></thead><tbody>';
 
 foreach ($result as $row) {
+	  $types = $row['type'];
+
+if(false !== strpos($types, 'Green')){
+  $types = $typeG;
+}
+elseif (false !== strpos($types, 'Red')){
+  $types = $typeR;
+}
+elseif (false !== strpos($types, 'Orange')){
+  $types = $typeO;
+}
 	echo '<tr><td>' . $row['name'] . '</td>
 		<td>' . $row['date'] . '</td>
 		<td>' . $row['city'] . '</td>
         <td>' . $row['address'] . '</td>
-        <td>' . $row['type'] . '</td>
+        <td>' . $types . '</td>
 		<td class="hide-on-small-only"><a class="waves-effect waves-light btn green" href="marker.php?id=' . $row['id'] .'">Edit</a></td>
 		<td class="hide-on-small-only"><a class="waves-effect waves-light btn red" href="delete-marker.php?id=' . $row['id'] . '"
 			onclick="return confirm(\'Are you sure you want to delete this marker?\');">Delete</a></td>
