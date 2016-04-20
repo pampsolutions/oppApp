67c7108f8dbbcc42419d40b964a3a2402b8e5b38
PAMP Solutions <pampsolutions@outlook.com>
2016-04-20 15:47:23 -0400

    Type Update

--- dashboard.php
+++ dashboard.php
@@ -20,25 +20,20 @@ $sql = "SELECT * FROM markers ORDER BY id DESC LIMIT 10";
 $result = $conn->query($sql);
 
 echo '
-      <div class="row container">
-      <div class="col s12 l8">
-        <table class="bordered highlight card" style="table-layout: fixed; width: 100%">
-        <h2>Recently Added Markers</h2>
-        <thead><tr><th>Title</th><th>Date</th><th>Main Intersection</th><th>Details</th><th>Edit</th><th>Delete</th></tr></thead><tbody>';
+      <div class="row">
+      <div class="col s12 l7">
+        <table class="bordered highlight" style="table-layout: fixed; width: 100%">
+        <thead><tr class="heading"><th data-field="id">RECENT MARKERS</th><th></th><th></th><th><sub>10 most recent markers</sub></th><th></th><th></th></tr>        <tr><th>Name</th><th>Date</th><th>City</th><th>Description</th><th>Type</th></tr></thead><tbody>';
 
 foreach ($result as $row) {
 	echo '<tr><td>' . $row['name'] . '</td>
 		<td>' . $row['date'] . '</td>
 		<td>' . $row['city'] . '</td>
         <td>' . $row['address'] . '</td>
-		<td class="hide-on-small-only"><a class="waves-effect waves-light btn green" href="marker-dash.php?id=' . $row['id'] .'">Edit</a></td>
-		<td class="hide-on-small-only"><a class="waves-effect waves-light btn red" href="delete-dash.php?id=' . $row['id'] . '"
-			onclick="return confirm(\'Are you sure you want to delete this marker?\');">Delete</a></td>
-       <td class="hide-on-med-and-up"><a class="waves-effect waves-light btn-floating green" href="marker-dash.php?id=' . $row['id'] .'"><i class="material-icons">edit_mode</i></a></td>
-		<td class="hide-on-med-and-up"><a class="waves-effect waves-light btn-floating red" href="delete-dash.php?id=' . $row['id'] . '"
-			onclick="return confirm(\'Are you sure you want to delete this marker?\');"><i class="material-icons" id="del">delete</i></a></td>
-		</tr>';
+        <td>' . $row['type'] . '</td>'
+        
 }
+
 
 echo '</tbody></table></div>';
 
