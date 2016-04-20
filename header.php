4d1e2b58659241ef42e18a0b2db84ff0830f4cd4
Sanjeevan P <sanjeevan.premkumar@gmail.com>
2016-04-20 01:04:19 -0400

    Update header.php

--- header.php
+++ header.php
@@ -1,26 +1,23 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>OPP App</title>
-
-<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-<script type="text/javascript" src="https://maps.google.com/maps/api/js?sensor=false"></script>
-<!--Import Google Icon Font-->
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<!--Import materialize.css-->
-<link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
-<link type="text/css" rel="stylesheet" href="css/index.css" media="screen,projection" />
-<link rel="stylesheet" href="css/map.css" />
-<!--Let browser know website is optimized for mobile-->
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
+  <title>OPP App</title>
+  <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script type="text/javascript" src="https://maps.google.com/maps/api/js?sensor=false"></script>
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!--Import materialize.css-->
+  <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+  <link type="text/css" rel="stylesheet" href="css/index.css" media="screen,projection" />
+  <!--Let browser know website is optimized for mobile-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
 
 <body>
   <header class="navbar-fixed">
     <nav>
         <div class="nav-wrapper black">
-            <a href="#!" class="brand-logo"><img class="responsive-img" id="logo" src="/img/logo.png" /></a>
+            <a href="#!" class="brand-logo"><img class="responsive-img" id="logo" src="http://localhost:81/oppApp-master/img/logo.png" /></a>
             <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
             <ul class="hide-on-med-and-down right">
                 <!--<li class="search">
@@ -34,7 +31,7 @@
                 <li><a href="/logout.php" class="waves-effect waves-light btn">LOGOUT</a></li>
             </ul>
             <ul class="side-nav" id="mobile-demo">
-                <li><img src="/img/logo.png" class="responsive-img" class="mobile-logo" /></li>
+                <li><img src="http://localhost:81/oppApp-master/img/logo.png" class="responsive-img" class="mobile-logo" /></li>
                 <li><a href="/dashboard.php">DASHBOARD</a></li>
                 <li><a href="/index.php">MAP</a></li>
                 <li><a href="/userguide.php">USER GUIDE</a></li>
