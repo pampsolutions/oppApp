74e61808d8e4db391238434fa055815a8aba01c5
PAMP Solutions <pampsolutions@outlook.com>
2016-04-20 18:38:51 -0400

    Update dashboard v1.1

--- dashboard.php
+++ dashboard.php
@@ -19,6 +19,10 @@ $sql = "SELECT * FROM markers ORDER BY id DESC LIMIT 10";
 
 $result = $conn->query($sql);
 
+$typeO = '<span class="orange relative" style="padding:5px 20px; border-radius:100px;">' . $types . '</span>';
+$typeG = '<span class="green relative" style="padding:5px 20px; border-radius:100px;">' . $types . '</span>';
+$typeR = '<span class="red relative" style="padding:5px 20px; border-radius:100px;">' . $types . '</span>';
+
 echo '
       <div class="row container">
       <div class="col s12 l8">
@@ -27,11 +31,22 @@ echo '
         <thead><tr><th>Name</th><th>Date</th><th>City</th><th>Description</th><th>Type</th></tr></thead><tbody>';
 
 foreach ($result as $row) {
+	$types = $row['type'];
+
+if(false !== strpos($types, 'Green')){
+  $types = $typeG;
+}
+elseif (false !== strpos($types, 'Red')){
+  $types = $typeR;
+}
+elseif (false !== strpos($types, 'Orange')){
+  $types = $typeO;
+}
 	echo '<tr><td>' . $row['name'] . '</td>
 		<td>' . $row['date'] . '</td>
 		<td>' . $row['city'] . '</td>
         <td>' . $row['address'] . '</td>
-        <td>' . $row['type'] . '</td></tr>';
+        <td>' . $types . '</td></tr>';
         
 }
 
