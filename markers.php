bfbdb6e38429dda3e5cc33bbd5ba16340201c88b
Sanjeevan P <sanjeevan.premkumar@gmail.com>
2016-04-20 11:01:27 -0400

    Update markers.php

--- markers.php
+++ markers.php
@@ -1,8 +1,17 @@
-<?php ob_start(); 
+<?php ob_start();
 
 require_once('auth.php');
 
 require_once('header.php');
+
+?>
+
+<div id="markerLog">
+  <div class="row">
+    <h1>Dashboard</h1>
+  </div>
+
+<?php
 
 require_once('db.php');
 
@@ -11,10 +20,11 @@ $sql = "SELECT * FROM markers ORDER BY id DESC";
 $result = $conn->query($sql);
 
 echo '
-      <div class="row">
+      <div class="row container">
       <div class="col s12">
         <table class="bordered highlight" style="table-layout: fixed; width: 100%">
-        <thead><tr class="heading"><th data-field="id">MARKER</th><th></th><th></th><th></th><th class="hide-on-med-and-down"></th><th class="hide-on-med-and-down"></th><th></th><th></th></tr>        <tr><th>Name</th><th>Date</th><th>City</th><th>Description</th><th class="hide-on-med-and-down">Lat</th><th class="hide-on-med-and-down">Lng</th><th>Edit</th><th>Delete</th></tr></thead><tbody>';
+        <h2>Marker Log</h2>
+        <thead><tr><th>Name</th><th>Date</th><th>City</th><th>Description</th><th class="hide-on-med-and-down">Lat</th><th class="hide-on-med-and-down">Lng</th><th>Edit</th><th>Delete</th></tr></thead><tbody>';
 
 foreach ($result as $row) {
 	echo '<tr><td>' . $row['name'] . '</td>
@@ -24,11 +34,11 @@ foreach ($result as $row) {
         <td class="hide-on-med-and-down">' . $row['lat'] . '</td>
         <td class="hide-on-med-and-down">' . $row['lng'] . '</td>
 		<td class="hide-on-small-only"><a class="waves-effect waves-light btn green" href="marker.php?id=' . $row['id'] .'">Edit</a></td>
-		<td class="hide-on-small-only"><a class="waves-effect waves-light btn red" href="delete-marker.php?id=' . $row['id'] . '" 
+		<td class="hide-on-small-only"><a class="waves-effect waves-light btn red" href="delete-marker.php?id=' . $row['id'] . '"
 			onclick="return confirm(\'Are you sure you want to delete this marker?\');">Delete</a></td>
        <td class="hide-on-med-and-up"><a class="waves-effect waves-light btn-floating green" href="marker.php?id=' . $row['id'] .'"><i class="material-icons">edit_mode</i></a></td>
-		<td class="hide-on-med-and-up"><a class="waves-effect waves-light btn-floating red" href="delete-marker.php?id=' . $row['id'] . '" 
-			onclick="return confirm(\'Are you sure you want to delete this marker?\');"><i class="material-icons center-align">delete</i></a></td>     
+		<td class="hide-on-med-and-up"><a class="waves-effect waves-light btn-floating red" href="delete-marker.php?id=' . $row['id'] . '"
+			onclick="return confirm(\'Are you sure you want to delete this marker?\');"><i class="material-icons center-align">delete</i></a></td>
 		</tr>';
 }
 
@@ -36,6 +46,11 @@ echo '</tbody></table></div></div>';
 
 $conn = null;
 
+?>
+
+</div>
+
+<?php 
 require_once('footer.php');
 
 ob_flush(); ?>
